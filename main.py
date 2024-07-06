81971166f5312c3f029f1467520e1010a6631a81
kodlandkev <olmoskevin987@gmail.com>
2024-07-06 13:22:26 -0600

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+lista_consejo=["La mayoría de las personas que sufren adicción tecnológica experimentan un fuerte estrés cuando se encuentran fuera del área de cobertura de la red o no pueden utilizar sus dispositivos",
+               "Según un estudio realizado en 2018, más del 50% de las personas de entre 18 y 34 años se consideran dependientes de sus smartphones",
+               "El estudio de la dependencia tecnológica es una de las áreas más relevantes de la investigación científica moderna",
+               "Una forma de combatir la dependencia tecnológica es buscar actividades que aporten placer y mejoren el estado de ánimo"]
+@app.route("/")
+def index():
+    return f'<h1>hola esta es mi pagina web y te dare consejos sobre la dependencia tecnologica</h1><a href="/nuevo consejo">¡Ver un dato aleatorio!</a>'
+
+@app.route("/nuevo consejo")
+def consejos():
+    return f'<p>{random.choice(lista_consejo)}</p>'
+    
+
+app.run(debug=True)
+
